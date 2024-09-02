69cb1066bd8acc2218c57df4cdeac723ee7ee784
Your Name <you@example.com>
2024-09-02 11:58:41 -0400

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,11 +1,13 @@
 let count = 0; // Initialize count to 0
 
-function increaseCount() {
-    count++; // Increment the count by 1
-}
-
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+}
+
+function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Your Instagram followers count was reset!");
 }
 
 function increaseCount() {
