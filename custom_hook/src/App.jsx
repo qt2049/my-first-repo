76850be61d095ecb6a9701095a52755db1c4c613
Your name <you@example.com>
2024-12-03 11:48:33 -0500

    Add React custom hook

---
+++ custom_hook/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
+import './Components/FetchData.css';
+
+function App() {
+  return (
+ <>
+ <FetchYogaData/>
+ <hr/>
+ <FetchData/>
+ </>
+  )
+}
+
+export default App
