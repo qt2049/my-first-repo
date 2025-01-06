b3b6f2aebe37275c0723d243c689cf90946b1252
Your Name <you@example.com>
2025-01-06 13:09:22 -0500

    Added add to cart feature

--- ecommerce_rtk/src/Components/ProductList.jsx
+++ ecommerce_rtk/src/Components/ProductList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import './ProductList.css'; 
+import './ProductList.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { addItemToCart } from './CartSlice';
 
 const ProductList = () => {
 
@@ -8,6 +11,12 @@ const ProductList = () => {
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]); // State to store disabled products
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    //setDisabledProducts([...disabledProducts, product.id]); // Mark the product as disabled
+  };
 
   return (
     <div className="product-list">
@@ -15,8 +24,12 @@ const ProductList = () => {
       <ul className="product-list-items">
       {products.map(product => (
         <li key={product.id} className="product-list-item">
-        <span>{product.name} - ${product.price}</span>
-        <button>Add to Cart</button>
+          <span>{product.name} - ${product.price}</span>
+          <button
+            className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+            onClick={() => handleAddToCart(product)}
+            //disabled={disabledProducts.includes(product.id)} // Disable button if product is in disabledProducts
+          >Add to Cart</button>
         </li>
       ))}
       </ul>
